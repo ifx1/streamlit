40b2bfc26bbb742cecedf71741195b31fc743065
LjhAUMEM <llnu14702@gmail.com>
2025-06-15 13:58:00 +0000

    init

---
+++ a.sh
@@ -0,0 +1,163 @@
+uname -a
+
+# if [ ! -f "nezha-agent" ]; then
+#     curl -LO https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_linux_amd64.zip
+#     unzip nezha-agent_linux_amd64.zip nezha-agent
+#     rm nezha-agent_linux_amd64.zip
+# fi
+
+# if [ ! -f "cloudflared" ]; then
+#     curl -L -o cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+# fi
+
+# if [ ! -f "xray" ]; then
+#     curl -LO https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+#     unzip Xray-linux-64.zip xray
+#     rm Xray-linux-64.zip
+# fi
+
+for pid_path in /proc/[0-9]*/; do
+    if [ -r "${pid_path}cmdline" ]; then
+        pid="${pid_path//[^0-9]/}"
+        cmdline_file="${pid_path}cmdline"
+        cmdline_output=$(xargs -0 < "$cmdline_file" 2>/dev/null)
+        if echo "$cmdline_output" | grep -q "nezha-agent"; then
+            kill -9 "$pid"
+        fi
+        if echo "$cmdline_output" | grep -q "xray"; then
+            kill -9 "$pid"
+        fi
+        if echo "$cmdline_output" | grep -q "cloudflared"; then
+            kill -9 "$pid"
+        fi
+    fi
+done
+
+sleep 1
+
+###########################################
+
+export NZ_SERVER=""
+export NZ_CLIENT_SECRET=""
+export NZ_TLS="true"
+export NZ_INSECURE_TLS="false"
+export NZ_DISABLE_AUTO_UPDATE="true"
+export NZ_UUID=""
+
+CF_TOKEN=""
+
+VLESS_PORT="3001"
+VLESS_DOMAIN=""
+VMESS_PORT="3002"
+VMESS_DOMAIN=""
+
+###########################################
+
+chmod +x nezha-agent
+./nezha-agent service -c config.yml install &>/dev/null
+nohup ./nezha-agent -c config.yml &>/dev/null &
+
+chmod +x cloudflared
+nohup ./cloudflared tunnel run --token "$CF_TOKEN" &>/dev/null &
+
+gen_xray_config() {
+    id=$(./xray uuid)
+    path=$(./xray uuid)
+    path=${path:0:8}
+    echo "{
+    \"inbounds\": [
+        {
+            \"listen\": \"::\",
+            \"port\": $VLESS_PORT,
+            \"protocol\": \"vless\",
+            \"settings\": {
+                \"clients\": [
+                    {
+                        \"id\": \"$id\"
+                    }
+                ],
+                \"decryption\": \"none\"
+            },
+            \"streamSettings\": {
+                \"network\": \"xhttp\",
+                \"security\": \"none\",
+                \"xhttpSettings\": {
+                    \"path\": \"$path\"
+                }
+            }
+        },
+        {
+            \"listen\": \"::\",
+            \"port\": $VMESS_PORT,
+            \"protocol\": \"vmess\",
+            \"settings\": {
+                \"clients\": [
+                    {
+                        \"id\": \"$id\"
+                    }
+                ]
+            },
+            \"streamSettings\": {
+                \"network\": \"xhttp\",
+                \"security\": \"none\",
+                \"xhttpSettings\": {
+                    \"path\": \"$path\"
+                }
+            }
+        }
+    ],
+    \"outbounds\": [
+        {
+            \"protocol\": \"freedom\"
+        }
+    ]
+}" > config.json
+
+    config="{
+  \"v\": \"2\",
+  \"ps\": \"vmess xhttp\",
+  \"add\": \"ip.sb\",
+  \"port\": \"80\",
+  \"id\": \"$id\",
+  \"aid\": \"0\",
+  \"scy\": \"auto\",
+  \"net\": \"xhttp\",
+  \"type\": \"packet-up\",
+  \"host\": \"$VMESS_DOMAIN\",
+  \"path\": \"$path\",
+  \"tls\": \"\",
+  \"sni\": \"\",
+  \"alpn\": \"\",
+  \"fp\": \"\"
+}"
+    vmess_link="vmess://$(echo -n "$config" | base64 -w 0)"
+    vless_link="vless://${id}@ip.sb:443?encryption=none&security=tls&type=xhttp&host=${VLESS_DOMAIN}&path=${path}&mode=packet-up#vless xhttp"
+
+    echo -e "${vmess_link}\n${vless_link}" | base64 -w 0 > links.txt
+}
+
+chmod +x xray
+gen_xray_config
+nohup ./xray &>/dev/null &
+
+####################################################
+
+for pid_path in /proc/[0-9]*/; do
+    if [ -r "${pid_path}cmdline" ]; then
+        pid="${pid_path//[^0-9]/}"
+        echo -n "${pid}: "
+        xargs -0 < "${pid_path}cmdline"
+        echo
+    fi
+done
+
+commands=("curl" "unzip" "ps" "ip" "hostname" "pkill" "grep" "openssl" "base64")
+for cmd in "${commands[@]}"; do
+    echo -n "$cmd "
+    if command -v "$cmd" &> /dev/null; then
+        echo "存在"
+    else
+        echo "不存在"
+    fi
+    echo
+done
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,42 @@
 import streamlit as st
+import subprocess
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+HOST = 'localhost'
+PORT = 3000
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+result = subprocess.run(["bash", "a.sh"], capture_output=True, text=True)
+st.write(f"result stdout: {result.stdout}")
+st.write(f"result stderr: {result.stderr}")
+
+class CustomHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            with open("links.txt", 'rb') as f:
+                file_content = f.read()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(file_content)
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(f"error: {e}".encode('utf-8'))
+
+def run_custom_server():
+    try:
+        server = HTTPServer((HOST, PORT), CustomHandler)
+        server.serve_forever()
+    except Exception as e:
+        pass
+
+server_thread = threading.Thread(target=run_custom_server)
+server_thread.daemon = True
+server_thread.start()
